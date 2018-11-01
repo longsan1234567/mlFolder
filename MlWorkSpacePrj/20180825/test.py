4bb5ede35582dec0bbe21c7a2b168193a9328ffc
long <1163820757@qq.com>
2018-11-01 22:26:01 +0800

    22

--- MlWorkSpacePrj/20180825/test.py
+++ MlWorkSpacePrj/20180825/test.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2018/11/1 下午10:02
+# @Time    : 2018/11/1 下午10:25
 # @Author  : scl
 # @Email   : 1163820757@qq.com
 # @File    : test.py
 # @Software: PyCharm
 
+import numpy as  np
 
-print("你好")
-
+print('你好')
